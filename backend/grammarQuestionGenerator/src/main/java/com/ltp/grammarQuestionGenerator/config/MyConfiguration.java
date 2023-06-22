47ee695830081961a833edc2e8c86ff2eae67b9f
tylercausey <tylercausey1219@gmail.com>
2023-06-22 02:44:32 -0500

    created docker-compose file for docker containers, edited files for appropriate routing and docker information

--- backend/grammarQuestionGenerator/src/main/java/com/ltp/grammarQuestionGenerator/config/MyConfiguration.java
+++ backend/grammarQuestionGenerator/src/main/java/com/ltp/grammarQuestionGenerator/config/MyConfiguration.java
@@ -3,14 +3,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// configures CORS settings to allow HTTP requests from localhost 3000 with all methods to all endpoints.
+// configures CORS settings to allow HTTP requests from designated origin with all methods to all endpoints.
 @Configuration
 public class MyConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") 
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+                .allowedOrigins("*") 
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*");
     }
 }
